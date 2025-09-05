7715dc7d7b8e8f0b4d2a74cfe6a8d34435fe1875
Omid Emadi <E.omid34@gmail.com>
2025-09-05 10:58:20 +0000

    it doesn't work again. fix it

--- src/lib/icon-map.ts
+++ src/lib/icon-map.ts
@@ -1,4 +1,5 @@
-import { Briefcase, Heart, Dumbbell, Wallet, BookOpen, Lightbulb, BrainCircuit, Target, PencilRuler, LineChart, Code, Palette, Music, Clapperboard, Gamepad2, Plane, Utensils, MessageCircle, Users, GitBranch, Archive, MapOff, type LucideIcon } from 'lucide-react';
+import { Briefcase, Heart, Dumbbell, Wallet, BookOpen, Lightbulb, BrainCircuit, Target, PencilRuler, LineChart, Code, Palette, Music, Clapperboard, Gamepad2, Plane, Utensils, MessageCircle, Users, GitBranch, Archive, MapOff } from 'lucide-react';
+import type { LucideIcon } from 'lucide-react';
 
 export const iconMap: { [key: string]: LucideIcon } = {
   Briefcase,
